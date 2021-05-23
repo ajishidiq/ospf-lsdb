811754c3ac09849a791abf213f48abbc6d4a547a
ajishidiq <ajishidiq@gmail.com>
2021-05-24 04:02:08 +0700

    test telnet

--- test1.py
+++ test1.py
@@ -7,19 +7,34 @@ from connect import (
 from sys import argv
 
 import time
+import textfsm
 
 start_time = time.time()
-host = argv[1]
-auth = argv[2]
-commands = ['show ip int brief',
-            'show int description',
-            'show ip ospf neighbor'
-           ]
+src = argv[1]
+dst = argv[2]
+auth = argv[3]
+#commands = ['show ip int brief',
+#            'show int description',
+#            'show ip ospf neighbor'
+#           ]
+commands = [f'traceroute mpls ipv4 {dst}/32 sou {src}']
 if auth == 'tacacs':
-    result = TacacsTelnetTo(host, commands)
+    result = TacacsTelnetTo(src, commands)
 elif auth == 'j2m':
-    result = j2mTelnetTo(host, commands)
+    result = j2mTelnetTo(src, commands)
 
-print(result)
+output = result[commands[0]].rstrip()
+
+with open('trace.template') as template:
+    fsm = textfsm.TextFSM(template)
+    results = fsm.ParseText(output)
+
+for result in results:
+    if(not result[2]):
+        MTU = 0
+    else:
+        MTU = result[2]
+    print(f'Hop #{result[0]} IP P2P {result[1]} MTU {MTU}')
+
 duration = time.time() - start_time
 print('it takes about {:.2f} sec'.format(duration))
